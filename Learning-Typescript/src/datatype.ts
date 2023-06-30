28c71a10276c049f64b2140fbbb0fc70ee344164
onlyVishesh <Visheshmaurya14@gmail.com>
2023-06-30 23:50:57 +0530

    Added Learning-Typescript folder that contain some basic typescript code

---
+++ Learning-Typescript/src/datatype.ts
@@ -0,0 +1,29 @@
+let sales: number = 2_321_234;
+let course: string = 'Typescript';
+let is_published: boolean = true;
+
+// or 
+// same thing is static declaration is done
+
+let sales1 = 2_321_234;
+let course1 = 'Typescript';
+let is_published1 = true;
+
+
+// dynamic declaration
+
+let cost; // datatype any and it can have any value 
+// No problem as js
+
+cost = 20;
+cost = 'twenty'
+
+// arrays
+let number = [1,2,'3']; //js
+let no: number[] = [1, 2, 3]
+// or
+let no1 = []
+
+ 
+
+
